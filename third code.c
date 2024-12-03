4384df8da89013bf1c9b28622e2a797c129b7e2a
Adrian joseph <adrianjoseph262006@gmail.com>
2024-12-03 11:23:21 +0530

    Add files via upload

---
+++ third code.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+char name [3];
+float price[3];
+int pages [3],i;
+printf ("enter names,priceand no: pages of 3 books"); for(i=0; i<=2; i++);
+scanf("%c%d", &name[i], &price[i],&pages[i]); printf(" and this is what you entered \n");
+for(i=0; i<=2; i++);
+printf("%cf%d\n", name[i],price[i],pages[i]);
+return 0;
+}
+W
+int main()
+{
+char name [3];
+float price[3];
+int pages [3],i
+printf ("enter names,priceand no: pages of 3 books"); for(i=0; i<=2; i++);
+scanf ("zczfzd", &name[i], &price[i],&pages[i]); printf (and this is what you entered \n"); for(i=0; i<=2; i++);
+printf (exfzd\n", name[i],price[i],pages[i]); return 0;
+}
